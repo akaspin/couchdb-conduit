de3d1ab99fab03c9f086126535442ec8715d0d7b
Alexander Dorofeev <aka.spin@gmail.com>
2012-01-03 15:30:23 +0500

    couchPut implemented

--- src/Database/CouchDB/Conduit/Explicit.hs
+++ src/Database/CouchDB/Conduit/Explicit.hs
@@ -1,10 +1,13 @@
-
+{-# LANGUAGE OverloadedStrings #-}
 module Database.CouchDB.Conduit.Explicit (
-    couchGet
+    couchGet,
+    couchPut
 ) where
 
 
 import qualified Data.ByteString as B
+import qualified Data.Text.Encoding as TE
+import qualified Data.HashMap.Lazy as M
 import qualified Data.Aeson as A
 
 import Data.Conduit (ResourceIO, runResourceT, ($$), resourceThrow)
@@ -26,6 +29,22 @@ couchGet p q = do
             (H.RequestBodyBS B.empty)
     either resourceThrow return $ valToObj res
 
+-- | Put an object in Couch DB with revision, returning the new Revision.
+couchPut :: (MonadCouch m, A.ToJSON a) => 
+        DocPath     -- ^ Document path.
+     -> Revision    -- ^ Document revision. For new docs provide empty string.
+     -> HT.Query    -- ^ Query arguments.
+     -> a           -- ^ The object to store.
+     -> m Revision      
+couchPut p r q val = do
+    v <- runResourceT $ couch HT.methodPut p (ifMatch r) q 
+            (protect syncJSON)
+            (H.RequestBodyLBS $ A.encode val)
+    either resourceThrow return (valToObj v >>= objToRev)
+  where 
+    ifMatch "" = []
+    ifMatch rv = [("If-Match", rv)]
+
 -- | Basic consumer for json            
 syncJSON :: ResourceIO m => H.ResponseConsumer m A.Value
 syncJSON _status _hdrs bsrc = bsrc $$ sinkParser A.json
@@ -34,3 +53,10 @@ syncJSON _status _hdrs bsrc = bsrc $$ sinkParser A.json
 valToObj :: A.Value -> Either CouchError A.Object
 valToObj (A.Object o) = Right o
 valToObj _ = Left $ CouchError Nothing "Couch DB did not return an object"
+
+-- | Converts an object to a revision
+objToRev :: A.Object -> Either CouchError Revision
+objToRev o = case M.lookup "rev" o of
+    (Just (A.String r)) -> Right $ TE.encodeUtf8 r
+    _  -> Left $ CouchError Nothing "unable to find revision"  
+
