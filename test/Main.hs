4ec73364caba2ace376731df18a5fc47121aae55
Alexander Dorofeev <aka.spin@gmail.com>
2012-01-22 05:05:14 +0500

    More tests.
    Signed-off-by: Alexander Dorofeev <aka.spin@gmail.com>

--- test/Main.hs
+++ test/Main.hs
@@ -3,14 +3,16 @@ module Main (main) where
 
 import Test.Framework (defaultMain)
 
+import qualified Database.CouchDB.Conduit.Test.Base
 import qualified Database.CouchDB.Conduit.Test.Explicit
 import qualified Database.CouchDB.Conduit.Test.Generic
 import qualified Database.CouchDB.Conduit.Test.View
 
 main :: IO ()
 main = defaultMain [
-    Database.CouchDB.Conduit.Test.Explicit.tests,
-    Database.CouchDB.Conduit.Test.Generic.tests,
-    Database.CouchDB.Conduit.Test.View.tests
+      Database.CouchDB.Conduit.Test.Base.tests
+    , Database.CouchDB.Conduit.Test.Explicit.tests
+    , Database.CouchDB.Conduit.Test.Generic.tests
+    , Database.CouchDB.Conduit.Test.View.tests
     ]
 
