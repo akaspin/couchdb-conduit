23cc6cd89e2fe62bb9adf7a89fd06fc826d963fb
Alexander Dorofeev <aka.spin@gmail.com>
2012-07-28 15:06:07 +0600

    Enable tests
    Signed-off-by: Alexander Dorofeev <aka.spin@gmail.com>

--- test/Main.hs
+++ test/Main.hs
@@ -11,10 +11,10 @@ import qualified Database.CouchDB.Conduit.Test.Query
 
 main :: IO ()
 main = defaultMain [
---    Database.CouchDB.Conduit.Test.Base.tests,
---    Database.CouchDB.Conduit.Test.Explicit.tests,
---    Database.CouchDB.Conduit.Test.Generic.tests,
---    Database.CouchDB.Conduit.Test.View.tests,
+    Database.CouchDB.Conduit.Test.Base.tests,
+    Database.CouchDB.Conduit.Test.Explicit.tests,
+    Database.CouchDB.Conduit.Test.Generic.tests,
+    Database.CouchDB.Conduit.Test.View.tests,
     Database.CouchDB.Conduit.Test.Query.tests
     ]
 
